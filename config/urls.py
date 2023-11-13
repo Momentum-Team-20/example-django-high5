c1531e435ee37ad0af1b899480992080edd0504c
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-11-13 15:31:15 -0500

    add homepage

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('registration.backends.simple.urls'))
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('', include('highfives.urls')),
 ]
